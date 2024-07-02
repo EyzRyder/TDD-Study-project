a5d4e7249de9dd0f8debd76ba0f56161633a36fd
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-07-01 23:05:13 -0300

    feat: add controler, router, model,

--- store/schemas/product.py
+++ store/schemas/product.py
@@ -9,3 +9,26 @@ class ProductBase(BaseSchemaMixin):
     price: Decimal = Field(..., description="Product price")
     status: bool = Field(..., description="Product status")
 
+class ProductIn(ProductBase, BaseSchemaMixin):
+    ...
+
+
+class ProductOut(ProductIn, OutSchema):
+    ...
+
+
+def convert_decimal_128(v):
+    return Decimal128(str(v))
+
+
+Decimal_ = Annotated[Decimal, AfterValidator(convert_decimal_128)]
+
+
+class ProductUpdate(BaseSchemaMixin):
+    quantity: Optional[int] = Field(None, description="Product quantity")
+    price: Optional[Decimal_] = Field(None, description="Product price")
+    status: Optional[bool] = Field(None, description="Product status")
+
+
+class ProductUpdateOut(ProductOut):
+    ...
